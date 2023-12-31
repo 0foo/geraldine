35137aacd574c40c56bdec6073408b96f26e663e
0foo <0foo@0foo.dev>
2023-12-31 15:17:53 -0600

    update

--- geraldine/plugins/jinja_file_parser.plugin.py
+++ geraldine/plugins/jinja_file_parser.plugin.py
@@ -16,8 +16,8 @@ def geraldine(in_data):
     try:
         json_file_path = util.find_file(template_dir, json_path) # the json file
     except Exception as e:
-         print(f"Cant find json data in front matter of: {source_path}")
-
+        print(f"Cant find json data in front matter of: {source_path}")
+        return
 
     filename_key = frontmatter["filename_key"]
     content = in_data["template_content_string"]
