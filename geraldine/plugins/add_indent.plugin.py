36b4a3e5fc9fc9d2dd6d8415bf2776f2d7a4125a
0foo <0foo@0foo.dev>
2023-12-31 19:34:09 -0600

    update

--- geraldine/plugins/add_indent.plugin.py
+++ geraldine/plugins/add_indent.plugin.py
@@ -11,6 +11,7 @@ from geraldine import util
 remove_extensions=['jinja']
 
 def add_indent_to_front(text, spaces_count=4):
+    print(text)
     spaces = ' ' * spaces_count
     lines = text.splitlines()
     lines_with_spaces = [spaces + line for line in lines]
@@ -20,7 +21,6 @@ def add_indent_to_front(text, spaces_count=4):
 def geraldine(processor_data):
     frontmatter = processor_data["frontmatter"]
     content = processor_data["template_content_string"]
-    print(content)
 
     if "indent_count" in frontmatter:
         indent_count = frontmatter["indent_count"]
