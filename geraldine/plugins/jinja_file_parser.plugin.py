eedf37de33c2cdec41d763628e0fa3bea931d8d7
0foo <0foo@0foo.dev>
2023-12-31 19:18:47 -0600

    update

--- geraldine/plugins/jinja_file_parser.plugin.py
+++ geraldine/plugins/jinja_file_parser.plugin.py
@@ -16,6 +16,7 @@ def module_apply(processor_data):
 
     if not isinstance(processor_list, list):
         processor_list = [processor_list]
+        processor_list.remove("jinja_file_parser")
     
     for processor in processor_list:
         if processor in modules:
@@ -68,7 +69,6 @@ def geraldine(in_data):
                 filename = util.dict_lookup_function(dict_item, filename_key.split("."))
             merged_template = jinja_template.render(dict_item)
             in_data["template_content_string"] = merged_template
-            in_data["frontmatter"]["processor"].remove("jinja_file_parser")
             final_content = module_apply(in_data)
             filename = filename + destination_extension
             destination_location = os.path.join(destination_dir, filename)
